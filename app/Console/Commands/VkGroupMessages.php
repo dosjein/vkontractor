2433d8efa134f22628bbd1c6b11e67ac743db363
Ronalds Sovas <sovas@greenbox.lv>
2019-08-28 23:05:58 +0300

    added new method usage

--- app/Console/Commands/VkGroupMessages.php
+++ app/Console/Commands/VkGroupMessages.php
@@ -101,79 +101,68 @@ class VkGroupMessages extends Command
 
         $groupUpdates = $this->requestGET([] , $jsapi.'/api/v1/vk_groups_updates');
 
-        foreach ($groupUpdates['data'] as $key => $groupData) {
-            try {
+        $this->info(json_encode($groupUpdates));
 
-                $this->info('Wall for '.$groupData['id'].' '.(isset($groupData['id'])? $groupData['id'] : 'NoName'));
+        if (is_array($groupUpdates)){
+            foreach ($groupUpdates['data'] as $key => $groupData) {
+                try {
 
-                if (isset($groupData['error'])){
-                    $this->error($groupData['error']);
-                    continue;
-                }
+                    $this->info('Wall for '.$groupData['id'].' '.(isset($groupData['id'])? $groupData['id'] : 'NoName'));
 
-                if (!isset($groupData['group_id'])){
-                    $this->error('Missing Group ID');
-                    continue;
-                }
-
-                foreach($vk->request('wall.get', ['owner_id' => $groupData['group_id']  , 'count' => 2])->batch(5) as $data){
-
-                    sleep(2);
-                    if ($data->count == 0){
+                    if (isset($groupData['error'])){
+                        $this->error($groupData['error']);
                         continue;
                     }
 
-                    $this->info('Total to porcess '.$data->count);
-
-                    if (!$data->data){
+                    if (!isset($groupData['group_id'])){
+                        $this->error('Missing Group ID');
                         continue;
                     }
 
-                    foreach (array_slice($data->data, 0, 3) as $key => $wallRecord) {
+                    foreach($vk->request('wall.get', ['owner_id' => $groupData['group_id']  , 'count' => 2])->batch(5) as $data){
 
-                        $messageRequest = Messages::where('user_id' , 'https://vk.com/feed?w=wall-'.$groupData['id'].'_'.$wallRecord->id);
+                        sleep(2);
+                        if ($data->count == 0){
+                            continue;
+                        }
 
-                        if ($messageRequest->count() == 0){
+                        $this->info('Total to porcess '.$data->count);
 
-                            try {
+                        if (!$data->data){
+                            continue;
+                        }
 
-                                $response = $client->get('https://api.chucknorris.io/jokes/random', array());
-                                $json = json_decode($response->getBody(true)->getContents() , true);
+                        foreach (array_slice($data->data, 0, 3) as $key => $wallRecord) {
 
-                                if (!(json_last_error() == JSON_ERROR_NONE && is_array($json))) {
-                                    $this->error('Response Error');
+                            $messageRequest = Messages::where('user_id' , 'https://vk.com/feed?w=wall-'.$groupData['id'].'_'.$wallRecord->id);
+
+                            if ($messageRequest->count() == 0){
+
+                                try {
+
+                                    $response = $client->get('https://api.chucknorris.io/jokes/random', array());
+                                    $json = json_decode($response->getBody(true)->getContents() , true);
+
+                                    if (!(json_last_error() == JSON_ERROR_NONE && is_array($json))) {
+                                        $this->error('Response Error');
+                                        continue;
+                                    }else{
+                                        $message = $json['value'];
+                                    }
+
+                                } catch (BadResponseException $ex) {
+                                    $error =  array('error' => 1 , 'details' => 'problems : '.$ex->getResponse()->getBody()); 
+
+                                    $this->error(json_encode($error));
                                     continue;
-                                }else{
-                                    $message = $json['value'];
-                                }
+                                }  
 
-                            } catch (BadResponseException $ex) {
-                                $error =  array('error' => 1 , 'details' => 'problems : '.$ex->getResponse()->getBody()); 
+                                if ($this->argument('psycho')){
+                                        try {
 
-                                $this->error(json_encode($error));
-                                continue;
-                            }  
-
-                            if ($this->argument('psycho')){
-                                    try {
-
-                                        $options = array('query' => array(
-                                            'IDENT' => Config::get('app.chatbot_token'),
-                                            'IN' => $message
-                                        ));
-
-                                        $response = $client->get(Config::get('app.chatbot_url'), $options);
-                                        $json = json_decode($response->getBody(true)->getContents() , true);
-
-                                        if (!(json_last_error() == JSON_ERROR_NONE && is_array($json))) {
-                                            $this->error('Response Error');
-                                            continue;
-                                        }
-
-                                        if (!isset($json['message'])){
                                             $options = array('query' => array(
                                                 'IDENT' => Config::get('app.chatbot_token'),
-                                                //'IN' => $message
+                                                'IN' => $message
                                             ));
 
                                             $response = $client->get(Config::get('app.chatbot_url'), $options);
@@ -183,46 +172,61 @@ class VkGroupMessages extends Command
                                                 $this->error('Response Error');
                                                 continue;
                                             }
+
+                                            if (!isset($json['message'])){
+                                                $options = array('query' => array(
+                                                    'IDENT' => Config::get('app.chatbot_token'),
+                                                    //'IN' => $message
+                                                ));
+
+                                                $response = $client->get(Config::get('app.chatbot_url'), $options);
+                                                $json = json_decode($response->getBody(true)->getContents() , true);
+
+                                                if (!(json_last_error() == JSON_ERROR_NONE && is_array($json))) {
+                                                    $this->error('Response Error');
+                                                    continue;
+                                                }
+                                            }
+                                            $message = $json['message'];
+
+                                            $this->info('Normal?: '.$message);
+
+
+                                        } catch (BadResponseException $ex) {
+                                            $error =  array('error' => 1 , 'details' => 'problems : '.$ex->getResponse()->getBody()); 
+
+                                            $this->error(json_encode($error));
+                                            continue;
                                         }
-                                        $message = $json['message'];
-
-                                        $this->info('Normal?: '.$message);
+                                }
 
 
-                                    } catch (BadResponseException $ex) {
-                                        $error =  array('error' => 1 , 'details' => 'problems : '.$ex->getResponse()->getBody()); 
 
-                                        $this->error(json_encode($error));
-                                        continue;
-                                    }
+                                $this->createComment($vk , $groupData['group_id'] , $wallRecord->id , $message);
+                                $this->info('https://vk.com/feed?w=wall-'.$groupData['id'].'_'.$wallRecord->id);
+
+                                $messageObject = new Messages();
+                                $messageObject->user_id = 'https://vk.com/feed?w=wall-'.$groupData['id'].'_'.$wallRecord->id;
+                                $messageObject->in = 0;
+                                $messageObject->message = $message;
+                                $messageObject->save();
+                            }else{
+                                break;
                             }
 
-
-
-                            $this->createComment($vk , $groupData['group_id'] , $wallRecord->id , $message);
-                            $this->info('https://vk.com/feed?w=wall-'.$groupData['id'].'_'.$wallRecord->id);
-
-                            $messageObject = new Messages();
-                            $messageObject->user_id = 'https://vk.com/feed?w=wall-'.$groupData['id'].'_'.$wallRecord->id;
-                            $messageObject->in = 0;
-                            $messageObject->message = $message;
-                            $messageObject->save();
-                        }else{
-                            break;
+                            $this->info('Wait for me');
+                            sleep(40);
                         }
 
-                        $this->info('Wait for me');
-                        sleep(40);
-                    }
 
 
-
-                }  
-                         
-            } catch (Error $e) {
-                $this->error($e->getMessage());
+                    }  
+                             
+                } catch (Error $e) {
+                    $this->error($e->getMessage());
+                }
+                # code... 
             }
-            # code... 
         }
     }
 }
